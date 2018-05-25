1ccab4048559e6f1a08e73d77d283d5a71dde766
Frans Lundberg <frans.lundberg@assaabloy.com>
2018-05-25 14:20:17 +0200

    all warnings gone, removed TweetNaclFastTest.java

---
+++ src/dualsalt/tests/RunTests.java
@@ -0,0 +1,14 @@
+package dualsalt.tests;
+
+/**
+ * Executable class to run all tests.
+ */
+public class RunTests {
+    // 
+    // TODO fix scroll blindness. 
+    // 
+    public static void main(String[] args) {
+        DualSaltTest.main(args);
+        DualSaltRandomTest.main(args);
+    }
+}
